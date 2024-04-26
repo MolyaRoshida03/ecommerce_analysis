6a2d58396248b94725aa7e43478a5abd60fd0c21
molyaroshida <141609897+molyaroshida@users.noreply.github.com>
2024-04-26 19:42:51 +0530

    adding sql_functions

--- sql_functions.py
+++ sql_functions.py
@@ -15,7 +15,7 @@ load_dotenv()
 user = os.getenv('USER')
 password = os.getenv('PASSWORD')
 host = os.getenv('HOST')
-database = 'swiftmarket
+database = 'swiftmarket'
 
 # Establishing connection
 connection = mysql.connector.connect(user = user,
@@ -34,3 +34,9 @@ def read_query(query):
     return pd.DataFrame(data=rows, columns=cursor.column_names)
         
 
+if __name__=='__main__':
+       
+    query = 'Show tables;'
+    print(query)
+    df = read_query(query=query)
+    print(df)
